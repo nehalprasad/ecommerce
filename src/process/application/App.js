9cf9799c7e8cea9d19ab8a8664af9e2d8d3cdec6
prasadnehaal <nehal.prasad@luminos.software>
2024-09-09 23:18:05 +0530

    project setup

--- src/process/application/App.js
+++ src/process/application/App.js
@@ -1,19 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import {
+  Home,
+  About,
+  Men,
+  Women,
+  Kids,
+  Contact,
+  SignIn,
+  SignUp,
+} from "../../show/pages/public";
 import { Navbar } from "../../show/components";
-import { Home, About, Men, Women, Kids, Contact } from "../../show/pages";
+import { Cart } from "../../show/pages/private";
 
 const App = () => {
   return (
     <Router>
-        <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/men" element={<Men />} />
         <Route path="/women" element={<Women />} />
         <Route path="/kids" element={<Kids />} />
+        <Route path="/card" element={<Cart />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signIn" element={<SignIn />} />
       </Routes>
     </Router>
   );
--- src/show/pages/public/index.js
+++ src/show/pages/public/index.js
@@ -1,15 +1,19 @@
-import Men from "./Men";
+import Men from './Men'
 import Home from "./Home";
-import Kids from ".//Kids";
+import Kids from "./Kids";
 import Women from "./Women";
 import About from "./About";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 import Contact from "./Contact";
 
 export {
-    Men, 
+    Men,
     Home,
     Kids,
     Women,
     About,
+    SignIn,
+    SignUp,
     Contact
 }
