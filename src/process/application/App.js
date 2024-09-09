104044121bcb15107dcdd23c6401d3aefb09f4e3
prasadnehaal <nehal.prasad@luminos.software>
2024-09-10 01:53:16 +0530

    navbar

--- src/process/application/App.js
+++ src/process/application/App.js
@@ -10,11 +10,13 @@ import {
   SignIn,
   SignUp,
 } from "../../show/pages/public";
+import { Navbar } from "../../show/components";
 import { Cart } from "../../show/pages/private";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
